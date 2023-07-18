3db606b73cf0d27240c3ef53b9e6c4d9ae6c2325
natalia646 <nataxash46@gmail.com>
2023-07-18 13:56:51 +0300

    delete and comlete

--- src/components/List.js
+++ src/components/List.js
@@ -4,16 +4,39 @@ import Rename from "../components/assets/undo-svgrepo-com.svg";
 import Done from "../components/assets/done.svg";
 
 export default function List({ todos, setTodos }) {
+  const handeleDelet = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleComplete = (todo) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id == todo.id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <div className="todos-list">
       {todos.map((todo) => (
         <li key={todo.id} className="todo-item">
           {todo.title}
-         <div>
-              <img className="icons" src={Done}></img>
-              <img className="icons" src={Rename}></img>
-              <img className="icons" src={Delete}></img>
-         </div>
+          <div>
+            <img
+              className="icons"
+              src={Done}
+              onClick={() => handleComplete(todo)}
+            ></img>
+            <img className="icons" src={Rename}></img>
+            <img
+              className="icons"
+              src={Delete}
+              onClick={() => handeleDelet(todo)}
+            ></img>
+          </div>
         </li>
       ))}
     </div>

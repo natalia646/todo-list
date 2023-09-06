abea59c3a029d01bc9d21fe5827770c12f14eb3e
natalia646 <nataxash46@gmail.com>
2023-09-06 11:07:56 +0300

    finish

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Delete from "../components/assets/delete.svg";
-import Rename from "../components/assets/undo-svgrepo-com.svg";
+import Change from "../components/assets/change.svg";
 import Done from "../components/assets/done.svg";
 
 export default function List({ todos, setTodos, setEditTodo }) {
@@ -11,7 +11,7 @@ export default function List({ todos, setTodos, setEditTodo }) {
   const handleComplete = (todo) => {
     setTodos(
       todos.map((item) => {
-        if (item.id == todo.id) {
+        if (item.id === todo.id) {
           return { ...item, completed: !item.completed };
         }
         return item;
@@ -27,20 +27,27 @@ export default function List({ todos, setTodos, setEditTodo }) {
   return (
     <div>
       {todos.map((todo) => (
-        <li key={todo.id} className={todo.completed ? 'todo-list complete' : 'todo-list'}>{todo.title}
+        <li
+          key={todo.id}
+          className={todo.completed ? "todo-list complete" : "todo-list"}
+        >
+          {todo.title}
 
           <div>
             <img
               className="icons"
+              alt="done"
               src={Done}
               onClick={() => handleComplete(todo)}
             ></img>
             <img
+              alt="rename"
               className="icons"
-              src={Rename}
+              src={Change}
               onClick={() => handleEdit(todo)}
             ></img>
             <img
+              alt="delete"
               className="icons"
               src={Delete}
               onClick={() => handeleDelet(todo)}
